8c02c1cf0e7ed04a580bfa0d1ca07180620f68de
Candela Yarossi <candyarossi@gmail.com>
2025-09-12 19:42:18 -0300

    Archivos base

---
+++ base/ManejoDeFechas.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public interface ManejoDeFechas {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    default LocalDate formatearFecha(String fecha) {
+        LocalDate date = LocalDate.parse(fecha, formatter);
+        return date;
+    }
+}
